09edad5468d63f5827180ba5466689cb9f5cd93a
EliseGupta <egupta20@nscds.org>
2020-02-12 21:12:59 +0000

    latest attempt

---
+++ letterproject2/letterproject2.js
@@ -0,0 +1,13 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var name = "";
+$('#fontbutton').click(
+    function(){
+        var textbox = $('#textbox').val();
+            console.log(textbox);
+        for(var i = 0; i<textbox.length; i++){
+            var letterIndex = alphabet.indexOf(textbox.charAt(i));
+            var filename = "letters/L_" + letterindex + ".png";
+            console.log(fileName);
+        }
+    }
+);
